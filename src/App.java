a9e74f5068414dc0bb35ea2dca933ee9ce24b188
Taisiya <lebei@mail.ru>
2023-07-29 20:10:06 +0300

    first commit

---
+++ src/App.java
@@ -0,0 +1,52 @@
+import java.util.Random;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        int[] array = new int[7];
+        for (int i = 0; i < array.length; i++)
+            array[i] = new Random().nextInt(10);
+        print(array);
+        heapSort(array);
+        print(array);
+    }
+
+    public static void heapSort(int arr[]) {
+        int n = arr.length;
+
+        for (int i = n / 2 - 1; i >= 0; i--)
+            heapify(arr, n, i);
+
+        for (int i = n - 1; i >= 0; i--) {
+            int temp = arr[0];
+            arr[0] = arr[i];
+            arr[i] = temp;
+
+            heapify(arr, i, 0);
+        }
+    }
+
+    public static void heapify(int arr[], int n, int i) {
+        int largest = i; // Инициализируем наибольший элемент как корень
+        int left = 2 * i + 1; // левый = 2*i + 1
+        int right = 2 * i + 2; // правый = 2*i + 2
+
+        if (left < n && arr[left] > arr[largest])
+            largest = left;
+        if (right < n && arr[right] > arr[largest])
+            largest = right;
+        if (largest != i) {
+            int swap = arr[i];
+            arr[i] = arr[largest];
+            arr[largest] = swap;
+
+            heapify(arr, n, largest);
+        }
+    }
+
+    public static void print(int[] array) {
+        for (int i = 0; i < array.length; i++)
+            System.out.print(array[i] + " ");
+        System.out.println();
+    }
+
+}
